3041cfcd30d3e75677cad4a0949dc747fe9b35de
cwaceke <40740586+cwaceke@users.noreply.github.com>
2018-07-01 19:08:30 +0000

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,8 +3,29 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+var height,width,color;
+
+$('#sizePicker').submit(function(event){
+  event.prevent(default);
+  height=$('#inputHeight');
+  width=$('#inputWidth');
+  
+  makeGrid(height,width);
+  
+})
+
+function makeGrid(x,y) {
 
 // Your code goes here!
+  
+  $("tr").remove();
+  
+  for (var i=1; i<=x; i++){
+    $('#pixelCanvas').append('<tr id = row '+i+'></tr>');
+    h
+    for(var j=1; j<y;j++){
+      $('#row'+i).append('<td></td>');
+    } 
+                             }
 
 }
